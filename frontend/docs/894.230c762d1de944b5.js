0113f1036d6b16db892fa8e00554cfe400f86fc5
MarcosAllysson <marcosgracip05@gmail.com>
2025-03-05 20:55:50 -0300

    preparing to deploy

---
+++ frontend/docs/894.230c762d1de944b5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksakai_ng=self.webpackChunksakai_ng||[]).push([[894],{9894:(h,s,n)=>{n.r(s),n.d(s,{HOME_ROUTES:()=>o});const o=[{path:"",loadComponent:()=>Promise.all([n.e(838),n.e(426),n.e(676)]).then(n.bind(n,4676)).then(t=>t.HomeComponent)}]}}]);
